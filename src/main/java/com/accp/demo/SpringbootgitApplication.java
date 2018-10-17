f47d55f591e1415fe96365fbed1dab08fb6b6b31
ywm <1173117445@qq.com>
2018-10-17 10:41:06 +0800

    第一次Eclipse提交

---
+++ src/main/java/com/accp/demo/SpringbootgitApplication.java
@@ -0,0 +1,12 @@
+package com.accp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgitApplication.class, args);
+	}
+}
